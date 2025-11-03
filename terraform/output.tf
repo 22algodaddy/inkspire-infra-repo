de9a9854e154f3d2ce1bb790a7336af23325dc19
DilipChoubey-Georgian <choubeydhruv2014@gmail.com>
2025-11-04 00:40:35 +0530

    testing oidc

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,14 +2,12 @@ output "ecr_repo_url" {
   value = aws_ecr_repository.react.repository_url
 }
 
-output "acm_cert_arn" {
-  value = aws_acm_certificate.cert.arn
-}
+# output "acm_cert_arn" {
+#   value = aws_acm_certificate.cert.arn
+# }
 
-output "eks_cluster_name" {
-  value = module.eks.cluster_id
-}
+# output "eks_cluster_name" {
+#   value = module.eks.cluster_id
+# }
 
-output "github_actions_role_arn" {
-  value = aws_iam_role.github_actions_role.arn
-}
+
