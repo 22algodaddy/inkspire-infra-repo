c93d0157c1571c10803b4d73389d6a41e69cef86
DilipChoubey-Georgian <choubeydhruv2014@gmail.com>
2025-11-04 00:39:25 +0530

    testing oidc

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -8,10 +8,10 @@ variable "project" {
   default = "react-devops"
 }
 
-variable "domain_name" {
-  description = "Fully qualified domain to use for the app (e.g. app.example.com)"
-  type        = string
-}
+# variable "domain_name" {
+#   description = "Fully qualified domain to use for the app (e.g. app.example.com)"
+#   type        = string
+# }
 
 # CIDR and basic VPC options (adjust to your environment)
 variable "vpc_cidr" { default = "10.0.0.0/16" }
