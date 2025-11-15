96f1e84086e62660c7bba4704d3fe4875a942d45
DilipChoubey-Georgian <choubeydhruv2014@gmail.com>
2025-11-15 18:17:58 +0530

    argocd file created

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -4,33 +4,3 @@ resource "aws_ecr_repository" "react" {
   tags = { Project = var.project }
 }
 
-resource "aws_ecr_lifecycle_policy" "react" {
-  repository = aws_ecr_repository.react.name
-  policy     = <<POLICY
-{
-  "rules": [
-    {
-      "rulePriority": 1,
-      "description": "Expire untagged images older than 30 days",
-      "selection": {
-        "tagStatus": "untagged",
-        "countType": "sinceImagePushed",
-        "countUnit": "days",
-        "countNumber": 30
-      },
-      "action": { "type": "expire" }
-    },
-    {
-      "rulePriority": 2,
-      "description": "Keep only the last 10 tagged images",
-      "selection": {
-        "tagStatus": "tagged",
-        "countType": "imageCountMoreThan",
-        "countNumber": 10
-      },
-      "action": { "type": "expire" }
-    }
-  ]
-}
-POLICY
-}
--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,8 +2,19 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name               = "inkspire-eks-cluster"
+  name               = "my-cluster"
   kubernetes_version = "1.33"
+
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+  }
 
   # Optional
   endpoint_public_access = true
@@ -11,13 +22,20 @@ module "eks" {
   # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
-  compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
-  }
+  vpc_id                   = aws_vpc.this.id
+  subnet_ids               = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 
-  vpc_id     = aws_vpc.this.id
-  subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+  example = {
+    ami_type       = "AL2023_x86_64_STANDARD"
+    instance_types = ["t3.small"]
+
+    min_size     = 1
+    max_size     = 2
+    desired_size = 1
+  }
+}
 
   tags = {
     Environment = "dev"
